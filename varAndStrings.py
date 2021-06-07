b6f8b82234a8f43e0d9e558eb4bd35d40e00317a
Deeya <bachanid23@greenhill.org>
2021-06-07 10:28:57 -0500

    first commit

---
+++ varAndStrings.py
@@ -0,0 +1,34 @@
+#Deeya Bachani
+#We are learning to learn with strings
+#With loops
+#Different type of var
+
+num1=19
+num2=3.5
+num3=123123123123
+print(type(num1))
+print(type(num2))
+print(type(num3))
+phrase="hello there"
+print(type[phrase])
+#string functions
+num=len(phrase)
+print(num)
+print(phrase[3:6]) #print from 3 to 6
+print(phrase[6:]) #from index to end
+print(phrase *2) #print in twice
+#concadenation --> joining strings
+print(phrase[2:num-2])
+while "there" in phrase:
+    print("there" in phrase)
+    phrase= "changed"
+phrase=phrase + "goodbye"
+print(phrase)
+#make 3 string variables and print them indivivdually
+#"print s1+s2"
+string1="good morning"
+string2="good afternoon"
+string3= "goodnight"
+print(string1+string2)
+print(string2+string3)
+print(string1+string3)

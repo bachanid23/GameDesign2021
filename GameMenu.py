142d4ba167c7ea8c24e7c918689b42547c641c89
Deeya <bachanid23@greenhill.org>
2021-06-07 22:42:16 -0500

    added going back to menu

--- GameMenu.py
+++ GameMenu.py
@@ -10,12 +10,22 @@ print("which one would you like to choose?")
 choice=input()
 if choice=="1":
     print("Welcome to level 1")
+    print()
+    print("which one would you like to choose next?")
+choice=input()
 if choice=="2":
     print("Welcome to level 2")
+    print()
+    print("which one would you like to choose nect?")
+choice=input()
 if choice=="3":
     print("Here are your scores!")
+    print()
+    print("which one would you like to choose next?")
+choice=input()
 if choice=="4":
     print("Thanks for playing! Goodbye!!")
+    
 
 
 

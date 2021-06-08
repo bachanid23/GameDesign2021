12559f3ed45734730741451af139ea572e4bab3d
Deeya <bachanid23@greenhill.org>
2021-06-07 22:23:48 -0500

    first commit

---
+++ GameMenu.py
@@ -0,0 +1,21 @@
+#Deeya Bachani
+#Game Menu assignment
+print("My game menu")
+print()
+print("1--level1")
+print("2--level2")
+print("3--Print Scores")
+print("4--Exit Game")
+print("which one would you like to choose?")
+choice=input()
+if choice=="1":
+    print("Welcome to level 1")
+if choice=="2":
+    print("Welcome to level 2")
+if choice=="3":
+    print("Here are your scores!")
+if choice=="4":
+    print("Thanks for playing! Goodbye!!")
+
+
+

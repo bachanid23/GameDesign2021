5e8be5ea7450845e7cd2036306e251cd77963010
Deeya <bachanid23@greenhill.org>
2021-06-09 10:34:46 -0500

    first loop within completed

--- NewGameMenu.py
+++ NewGameMenu.py
@@ -1,14 +1,16 @@
 #Deeya Bachani
 #While loop instead of if
 l1="**************************"
-l2="* My game *"
+l2="*        My game         *"
 l3="* *"
-l4="* 1 - Level1 *"
-l5="* 2 - Level2 *"
-l6="* 3 - Scores *"
-l7="* 4 - Exit Game *"
-l8="* 5 - Play with phrase *"
-l9="**************************"
+l4="* 1 - Capitalize *"
+l5="* 2 - Upper *"
+l6="* 3 - Lower *"
+l7="* 4 - Find index of char *"
+l8="* 5 - Split *"
+l9="* 6 - Translate *"
+l10="* 7 - Exit *"
+l11="**************************"
 x=1 #global variable
 def menu():
     print(l1)
@@ -20,6 +22,8 @@ def menu():
     print(l7)
     print(l8)
     print(l9)
+    print(l10)
+    print(l11)
     print("please enter your selection from 1 to 5")
     inputNumber = input()
     x = int(inputNumber)
@@ -33,25 +37,43 @@ def score():
     print("* 3 - ???- 745 *")
     print(l3)
     print(l9)
+def pause():
+    print("Press N to continue or Y to stay")
+    input()
+
+
 while x !=4: 
     x=menu()
     if(x==1): #if statement are selection or branching
-        print("Level 1 Chosen")
-        print("Please enter a phrase")
-        answer = input()
-        b=answer.upper()
-        print(b)
+        capitalize=True
+        while capitalize==True:
+            print("Level 1 Chosen")
+            print("Please enter a phrase")
+            answer = input()
+            print(answer.capitalize())
+            print("Do you want to convert to another string?")
+            print("Y/N")
+            answer = input()
+            if (answer== "Y"):
+                    continue
+            if (answer=="N"):
+                    capitalize=False
+            pause()
+
+        
+        
+
     if(x==2):
         print("Level 2 Chosen")
         print("Please enter a phrase")
         answer = input()
-        b=answer.capitalize()
+        b=answer.upper()
         print(b)
     if(x==3):
         score()
         print("Please enter a phrase")
         answer = input()
-        b=answer.swapcase()
+        b= answer.swapcase()
         print(b)
     if(x==5):
         print("play with phrase")

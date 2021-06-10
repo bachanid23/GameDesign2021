ace6908509cfd6954df467e158ee30c4b363e294
Deeya <bachanid23@greenhill.org>
2021-06-09 22:05:09 -0500

    Added staying or leavng each level

--- NewGameMenu.py
+++ NewGameMenu.py
@@ -5,10 +5,10 @@ l2="*        My game         *"
 l3="* *"
 l4="* 1 - Capitalize *"
 l5="* 2 - Upper *"
-l6="* 3 - Lower *"
-l7="* 4 - Find index of char *"
-l8="* 5 - Split *"
-l9="* 6 - Translate *"
+l6="* 3 - Swapcase *"
+l7="* 4 - Count *"
+l8="* 5 - myTuple *"
+l9="* 6 - Title *"
 l10="* 7 - Exit *"
 l11="**************************"
 x=1 #global variable
@@ -40,50 +40,104 @@ def score():
 def pause():
     print("Press N to continue or Y to stay")
     input()
-
-
-while x !=4: 
+while x !=7: 
     x=menu()
     if(x==1): #if statement are selection or branching
         capitalize=True
         while capitalize==True:
-            print("Level 1 Chosen")
+                print("Level 1 Chosen")
+                print("Please enter a phrase")
+                answer = input()
+                print(answer.capitalize())
+                print("Do you want to convert to another string?")
+                print("Y/N")
+                answer = input()
+                if (answer== "Y"):
+                        continue
+                if (answer=="N"):
+                        capitalize=False
+                pause()
+
+            
+        
+
+    if(x==2):
+        upper=True
+        while upper==True:
+            print("Level 2 Chosen")
             print("Please enter a phrase")
             answer = input()
-            print(answer.capitalize())
+            print(answer.upper())
             print("Do you want to convert to another string?")
             print("Y/N")
             answer = input()
             if (answer== "Y"):
-                    continue
+                        continue
             if (answer=="N"):
-                    capitalize=False
+                        upper=False
             pause()
 
-        
-        
-
-    if(x==2):
-        print("Level 2 Chosen")
-        print("Please enter a phrase")
-        answer = input()
-        b=answer.upper()
-        print(b)
     if(x==3):
-        score()
-        print("Please enter a phrase")
-        answer = input()
-        b= answer.swapcase()
-        print(b)
+        swapcase=True
+        while swapcase==True:
+            print("Level 3 Chosen")
+            print("Please enter a phrase")
+            answer = input()
+            print(answer.swapcase())
+            print("Do you want to convert to another string?")
+            print("Y/N")
+            answer = input()
+            if (answer== "Y"):
+                        continue
+            if (answer=="N"):
+                        swapcase=False
+            pause()
+    if(x==4):
+        count=True
+        while count==True:
+            print("Level 4 Chosen")
+            print("Please enter a phrase")
+            answer = input()
+            print(answer.count("i"))
+            print("Do you want to convert to another string?")
+            print("Y/N")
+            answer = input()
+            if (answer== "Y"):
+                        count=True
+            if (answer=="N"):
+                        count=False
+            pause()
     if(x==5):
-        print("play with phrase")
-        print("Please enter a phrase")
-        answer = input()
-        myTuple = ("Bob", "Joe", "Emma")
-        b=answer.join(myTuple)
-        print(b)
+        myTuple=True
+        while myTuple==True:
+            print("Level 5 chosen")
+            print("Please enter a phrase")
+            answer = input()
+            myTuple = ("Bob", "Joe", "Emma")
+            b=answer.join(myTuple)
+            print(b)
+            print("Do you want to convert to another string?")
+            print("Y/N")
+            answer = input()
+            if (answer== "Y"):
+                        myTuple=True
+            if (answer=="N"):
+                        myTuple=False
+            pause()
+    if(x==6):
+        title=True
+        while title==True:
+            print("Level 6 chosen")
+            print("Please enter a phrase")
+            answer = input()
+            print(answer.title())
+            print("Do you want to convert to another string?")
+            print("Y/N")
+            answer = input()
+            if (answer== "Y"):
+                        continue
+            if (answer=="N"):
+                        title=False
 print("Goodbye, Thank you for playing")
-print("Please enter a phrase")
-answer = input()
-b=answer.isdigit()
-print(b)
+
+

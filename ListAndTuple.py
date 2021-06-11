daa2f780cbaa34a3a388788d2f952db8caece6cd
Deeya <bachanid23@greenhill.org>
2021-06-11 09:31:12 -0500

    added tuple

--- ListAndTuple.py
+++ ListAndTuple.py
@@ -1,6 +1,9 @@
 #Deeya Bachani
 #We are learning about lists and tuples
 #Learn their functions and looping with list
+
+#How to use a module or library by importing
+import random
 from typing import Counter
 
 
@@ -13,7 +16,12 @@ for fruit in myFruit:
     print(fruit, end= " , ")
 print()
 Counter=len(myFruit) #length of your list is one more than your last index
+indx=random.randint(0,Counter-1)
+print("your lucky fruit is", myFruit[indx])
 
+#random method choice()
+word=random.choice(myFruit)
+print("your random fruit is", word)
 for x in range(0,Counter-1):
     print(myFruit[x], end= " , ")
 
@@ -27,4 +35,16 @@ myFruit.insert(0,"kiwi")
 myFruit.insert(2,"papaya")
 myFruit.append("beets")
 print(myFruit)
+
+fruity=("apple", "pears", "banana") #tuple
+print("tuple" , fruity)
+temp=list(fruity) #temp is a list
+print("list", temp)
+temp.insert(1, "kiwi")
+fruity=tuple(temp)
+print("tuple modified", fruity)
+print("list modified", fruity)
+for element in fruity:
+    print(element)
+
 

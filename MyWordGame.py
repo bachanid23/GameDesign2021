34f7d06f1b26a70116b122bddca5e99e579eeef2
Deeya <bachanid23@greenhill.org>
2021-06-14 23:31:10 -0500

    fixed bugs

--- MyWordGame.py
+++ MyWordGame.py
@@ -10,52 +10,63 @@
 import random
 from typing import Counter
 gamewords= ['python', 'java', 'trackpad', 'computer', 'keyboard','geeks','laptop','headphones','charger','mouse','software','hardware']
-#create menu
-answer=input("Do you want to guess the word?")
-name=input("What is your name?")
-answer=answer.upper()
+print("Word Game")
+print("1- Play")
+print("2- Scores")
+print("3- Exit")
+choice= input()
+while "1" in choice:
+    answer=input("Do you want to guess the word?")
+    name=input("What is your name?")
+    answer=answer.upper()
 
 
-def updateWord(word):
-     for char in word:
-            if char in guesses:
-                print(char, end='')
-            else:
-                print("_", end = " ")
-
-
-while "Y" in answer:
-    print("Good luck", name, "!")
-    word=random.choice(gamewords)
-    counter=len(word)
-    print(counter)
-    print(word)
-    turns=10 #should we consider controlling this number
-    guesses= ""
-    counter=len(word)
-    while turns>0 and counter >0:
+    def updateWord(word):
         for char in word:
             if char in guesses:
                 print(char, end='')
             else:
                 print("_", end = " ")
-        newGuess=input("\n\n Give me a letter ")
-        guesses +=newGuess
-        if newGuess in word:
-                newGuess=input("\n\n Give me a letter ")
-                characters=word.count(newGuess)
-                counter -=characters
-                print("nice guess")
-                guesses += newGuess
-        else:
-                turns -=1
-                print("wrong! you have", turns, "guesses left")
-        updateWord(word) 
-        print("wrong! you have", turns, "guesses left")
+
+    score=0
+    while "Y" in answer:
+        guesses= ""
+        print("Good luck", name, "!")
+        word=random.choice(gamewords)
         updateWord(word)
-    if Counter == 0:
+        counter=len(word)
+        turns=10 #should we consider controlling this number
+        counter=len(word)
+        while turns>0 and counter >0:
+            newGuess=input("\n\n Give me a letter ")
+            guesses +=newGuess
+            if newGuess in word:
+                    characters=word.count(newGuess)
+                    counter -=characters
+                    print("nice guess")
+            else:
+                    turns -=1
+                    print("wrong! you have", turns, "guesses left")
+            updateWord(word) 
+        print()
+        if counter == 0:
             print("You guessed right!")
-    else:
+            score +=1
+        else:
             print("Try again next time!")
-    answer=input("would you like to play again?").upper()
-    print(name, " thank you for playing")
+        answer=input("would you like to play again?").upper()
+if "2" in choice:
+    import os
+    import sys
+    filename="WordGameScores.py"
+    FILE=open(filename, 'r')
+    print(FILE.read())
+    FILE.close()
+
+if "3" in choice:
+    import os
+    import sys
+    filename="WordGameExit.py"
+    FILE=open(filename, 'r')
+    print(FILE.read())
+    FILE.close()

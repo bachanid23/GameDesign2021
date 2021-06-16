3cac73befb9e36c0689949808f85d154de659c6a
Deeya <bachanid23@greenhill.org>
2021-06-16 13:36:51 -0500

    first commit

--- myPygame.py
+++ myPygame.py
@@ -1,1 +1,15 @@
-import Pygame
+import myPygame, time, sys, pygame
+
+pygame.init()
+pygame.time.delay(100)
+WIDTH=600
+HEIGHT= 400
+#create an object to open window
+screen=pygame.display.set_mode((WIDTH,HEIGHT))
+
+check =True
+while check:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            check = False
+pygame.quit()

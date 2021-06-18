d658a9041c189a8446e6d2a9ffbf57a0dc52220f
Deeya <bachanid23@greenhill.org>
2021-06-18 10:32:22 -0500

    added code from class

--- myPygame.py
+++ myPygame.py
@@ -10,12 +10,17 @@ pygame.init()
 pygame.time.delay(100)
 WIDTH=600
 HEIGHT= 400
+#create the objects
+
+bg=pygame.image.load("Images/backGr.png")
+some=pygame.image.load("pycache/lego.png")
 white=[255, 255, 255]
 purple=[200,190,0]
 green=[50,25,255]
 #create an object to open window
 screen=pygame.display.set_mode((WIDTH,HEIGHT))
 screen.fill(purple)
+screen.blit(bg,(0,0))#create image on top of other one
 pygame.display.set_caption('My Game')#change title on screen and you can also change icon
 pygame.display.update() #command to actually do something
 
@@ -25,6 +30,14 @@ y=10
 rad=30
 hbox,wbox=20,20
 rect=pygame.Rect(x,y,hbox,wbox)#this creates a rectangle
+
+f=50
+j=40
+height,width=40,40
+rect2=pygame.Rect(f,j,height,width)
+
+Jumpcheck=False
+Jump=10
 
 
 while check:
@@ -39,24 +52,53 @@ while check:
     if keyBoardKey[pygame.K_RIGHT]:
         rect.x +=speed
         rad +=speed
-    if keyBoardKey[pygame.K_UP]: #moving up on y (-)
-        rect.y -=speed
-    if keyBoardKey[pygame.K_DOWN]:
-        rect.y +=speed
+#is to check if we are going to jump or not
+    if not Jumpcheck:
+        if keyBoardKey[pygame.K_UP]: #moving up on y (-)
+            rect.y -=speed
+        if keyBoardKey[pygame.K_DOWN]:
+            rect.y +=speed
+        if keyBoardKey[pygame.K_SPACE]:
+            Jumpcheck=True
+    else:
+        if Jump>=-10:
+            rect.y -=(Jump* abs(Jump))/2
+            Jump-=1
+        else:
+            Jump=10
+            Jumpcheck=False
     if keyBoardKey[pygame.K_s]:
         rad-=speed
     if keyBoardKey[pygame.K_f]:
         rad+=speed
-    if rect.x < 0: rect, x=0
+    if keyBoardKey[pygame.K_w]:
+        rect2.x -=speed
+    if keyBoardKey[pygame.K_r]:
+        rect2.x +=speed
+    if keyBoardKey[pygame.K_e]:
+        rect2.y -=speed
+    if keyBoardKey[pygame.K_d]:
+        rect2.y +=speed
+    if rect.x < 0: rect.x=0
     if rect.x > WIDTH-wbox: rect.x = WIDTH.wbox
-    if rect.y < 0: rect,rect.y=0
+    if rect.y < 0: rect.y=0
     if rect.y > HEIGHT-hbox: rect.y = HEIGHT.hbox
+    if rect2.x < 0: rect2.x=0
+    if rect2.x > WIDTH-width: rect2.x = WIDTH.width
+    if rect2.y < 0: rect2.y=0
+    if rect2.y > HEIGHT-height: rect2.y = HEIGHT.height
     screen.fill(purple)
     if rad < 0: rad=1
     if rad > WIDTH-x: rad=WIDTH-x
+    if rect.colliderect(rect2):
+        rect.x -=3
+        rect2.x -=3
+    screen.fill(purple)
+    screen.blit(bg,(0,0))
+    screen.blit(some, (rect.x,rect.y))
     pygame.draw.rect(screen,(green),rect)
     pygame.draw.circle(screen,(white), (x+300, y+200), rad,2)
-    pygame.draw.rect(screen,(green),rect)
+    pygame.draw.rect(screen,(green),rect2)
     pygame.draw.rect(screen,(255,0,0),rect)
     pygame.display.flip()
     pygame.time.delay(30)
